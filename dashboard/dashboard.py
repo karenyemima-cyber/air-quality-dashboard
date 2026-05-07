42672a123e91e7a28a82e526c63312cb1c583290
karenyemima-cyber <karenyemima@student.ub.ac.id>
2026-05-08 02:01:33 +0700

    Update dashboard.py
    Memperbaiki syntax untuk load data

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -40,18 +40,18 @@ sns.set_style("darkgrid")
 # LOAD DATA
 # =====================================================
 
-df1 = pd.read_csv("../data/PRSA_Data_Aotizhongxin_20130301-20170228.csv")
-df2 = pd.read_csv("../data/PRSA_Data_Changping_20130301-20170228.csv")
-df3 = pd.read_csv("../data/PRSA_Data_Dingling_20130301-20170228.csv")
-df4 = pd.read_csv("../data/PRSA_Data_Dongsi_20130301-20170228.csv")
-df5 = pd.read_csv("../data/PRSA_Data_Guanyuan_20130301-20170228.csv")
-df6 = pd.read_csv("../data/PRSA_Data_Gucheng_20130301-20170228.csv")
-df7 = pd.read_csv("../data/PRSA_Data_Huairou_20130301-20170228.csv")
-df8 = pd.read_csv("../data/PRSA_Data_Nongzhanguan_20130301-20170228.csv")
-df9 = pd.read_csv("../data/PRSA_Data_Shunyi_20130301-20170228.csv")
-df10 = pd.read_csv("../data/PRSA_Data_Tiantan_20130301-20170228.csv")
-df11 = pd.read_csv("../data/PRSA_Data_Wanliu_20130301-20170228.csv")
-df12 = pd.read_csv("../data/PRSA_Data_Wanshouxigong_20130301-20170228.csv")
+df1 = pd.read_csv("data/PRSA_Data_Aotizhongxin_20130301-20170228.csv")
+df2 = pd.read_csv("data/PRSA_Data_Changping_20130301-20170228.csv")
+df3 = pd.read_csv("data/PRSA_Data_Dingling_20130301-20170228.csv")
+df4 = pd.read_csv("data/PRSA_Data_Dongsi_20130301-20170228.csv")
+df5 = pd.read_csv("data/PRSA_Data_Guanyuan_20130301-20170228.csv")
+df6 = pd.read_csv("data/PRSA_Data_Gucheng_20130301-20170228.csv")
+df7 = pd.read_csv("data/PRSA_Data_Huairou_20130301-20170228.csv")
+df8 = pd.read_csv("data/PRSA_Data_Nongzhanguan_20130301-20170228.csv")
+df9 = pd.read_csv("data/PRSA_Data_Shunyi_20130301-20170228.csv")
+df10 = pd.read_csv("data/PRSA_Data_Tiantan_20130301-20170228.csv")
+df11 = pd.read_csv("data/PRSA_Data_Wanliu_20130301-20170228.csv")
+df12 = pd.read_csv("data/PRSA_Data_Wanshouxigong_20130301-20170228.csv")
 
 all_df = pd.concat([
     df1, df2, df3, df4, df5, df6,
